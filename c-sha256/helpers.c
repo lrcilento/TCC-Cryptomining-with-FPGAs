f929cf8f4af005b2c8234ac6b14054f7eee44277
lucasfacci <lucasfacci@protonmail.com>
2022-11-02 00:01:59 -0300

    preprocessMessage function added

--- c-sha256/helpers.c
+++ c-sha256/helpers.c
@@ -68,9 +68,11 @@ int *translate(char *message) {
     // It converts every Unicode code point to binary and chops off the '0b' binary indicator
     char *bytes[strlen(message)];
     int counter = 0;
-    for (int i = 0; i < sizeof(charCodes); i++) {
+    for (int i = 0; i < strlen(message); i++) {
         char *tempCode = bin(*(charCodes + i));
-        char *code = malloc(strlen(tempCode));
+        // printf("%s\n", tempCode);
+        // printf("%lu\n", strlen(tempCode));
+        char *code = malloc(8);
         
         for (int j = 0; j < strlen(tempCode); j++) {
             if (j >= 2) {
@@ -79,14 +81,19 @@ int *translate(char *message) {
             }
         }
         counter = 0;
+        // printf("%s\n", code);
 
         bytes[i] = zfill(code, 8);
+        // printf("%s\n", bytes[i]);
         free(code);
     }
     // It takes the binary strings and turns them into a list, every bit in a different index
-    int *bits = malloc(strlen(message) * 8);
+    int *bits = malloc((strlen(message) * 8) * sizeof(int));
     for (int i = 0; i < sizeof(bytes) / 8; i++) {
+        // printf("I: %d\n", i);
         for (int j = 0; j < strlen(bytes[i]); j++) {
+            // printf("J: %d\n", j);
+            // printf("%d\n", bytes[i][j] - '0');
             bits[counter] = bytes[i][j];
             counter++;
         }
@@ -96,13 +103,15 @@ int *translate(char *message) {
 }
 
 // It receives as an input an array of bits and a chunk lenght
-// It breaks the array of bits in chunks of bits and returns a pinter to an array containing these arrays of chunks of bits
-int *chunker(int *bits, int chunk_lenght) {
+// It breaks the array of bits in chunks of bits and returns a pointer to an array containing these arrays of chunks of bits
+int *chunker(int *bits, int bitsLenght, int chunk_lenght) {
     // It counts how much memory will be necessary to allocate
     int sizeToAllocate = 0;
-    int sizeToAllocateAux = 0;    
-    if ((*(&bits + 1) - bits) > chunk_lenght) {
-        for (int i = 0; i < *(&bits + 1) - bits; i++) {
+    int sizeToAllocateAux = 0;
+    // printf("%d\n", bitsLenght);
+    // printf("%d\n", chunk_lenght);
+    if (bitsLenght > chunk_lenght) {
+        for (int i = 0; i < bitsLenght; i++) {
             sizeToAllocateAux++;
             if (sizeToAllocateAux == chunk_lenght) {
                 sizeToAllocate++;
@@ -120,14 +129,14 @@ int *chunker(int *bits, int chunk_lenght) {
     int chunkedSize = sizeToAllocate;
     for (int i = 0; i < chunkedSize; i++) {
         if ((i == chunkedSize - 1) && (sizeToAllocateAux > 0)) {
-            chunked[i] = malloc(sizeToAllocateAux);
-        } else if ((*(&bits + 1) - bits) < chunk_lenght) {
-            chunked[i] = malloc(*(&bits + 1) - bits);
+            chunked[i] = malloc(sizeToAllocateAux * sizeof(int));
+        } else if (bitsLenght < chunk_lenght) {
+            chunked[i] = malloc(bitsLenght * sizeof(int));
         } else {
-            chunked[i] = malloc(chunk_lenght);
+            chunked[i] = malloc(chunk_lenght * sizeof(int));
         }
     }
-    // It points every "row/array" of the chunked array to the input data it has to have
+    // It points every "row/array" of the chunked array to the input data it needs to have
     int counter = 0;
     for (int i = 0; i < chunkedSize; i++) {
         chunked[i] = &bits[counter];
@@ -142,18 +151,18 @@ int *chunker(int *bits, int chunk_lenght) {
     //     int *p = chunked[i];
     //     if (i == chunkedSize - 1 && sizeToAllocateAux > 0) {
     //         for (int j = 0; j < sizeToAllocateAux; j++) {
-    //             printf("%d ", *p);
+    //             printf("%c ", *p);
     //             // Move the pointer to the next element
     //             p++;
     //         }
     //     } else {
     //         for (int j = 0; j < chunk_lenght; j++) {
-    //             printf("%d ", *p);
+    //             printf("%c ", *p);
     //             // Move the pointer to the next element
     //             p++;
     //         }
     //     }
-    //     printf("\n");
+    //     printf("\n\n");
     //     k++;
     //     // Move the pointer to the next row
     //     chunked[i]++;
@@ -164,16 +173,18 @@ int *chunker(int *bits, int chunk_lenght) {
 }
 
 // It fills the array with 0's if its length is smaller than the input lenght until it's equal to the input lenght
+// The zeros (48) and ones (49) are in ASCII
 int *fillZeros(int *bits, int bitsLenght, int desiredLenght, char *endian) {
-    // int l = *(&bits + 1) - bits;
+    if (bitsLenght > desiredLenght) {
+        return bits;
+    }
     int zerosToAdd = desiredLenght - bitsLenght;
-    // printf("%lu\n", *(&bits + 1) - bits);
-    int *bitsOutput = malloc(sizeof(int) * desiredLenght);
+    int *bitsOutput = malloc(desiredLenght * sizeof(int));
     // If endian is equal to "LE" (little-endian) it will insert the zeros in the end of the array
     if (strcmp(endian, "LE") == 0) {
         for (int i = 0; i < desiredLenght; i++) {
             if (i >= bitsLenght) {
-                *(bitsOutput + i) = 0;
+                *(bitsOutput + i) = 48;
             } else {
                 *(bitsOutput + i) = *(bits + i);
             }
@@ -183,7 +194,7 @@ int *fillZeros(int *bits, int bitsLenght, int desiredLenght, char *endian) {
         int counter = 0;
         for (int i = 0; i < desiredLenght; i++) {
             if (i < zerosToAdd) {
-                *(bitsOutput + i) = 0;
+                *(bitsOutput + i) = 48;
             } else {
                 *(bitsOutput + i) = *(bits + counter);
                 counter++;
@@ -191,5 +202,180 @@ int *fillZeros(int *bits, int bitsLenght, int desiredLenght, char *endian) {
         }
     }
 
-    return bits;
+    return bitsOutput;
+}
+
+// It returns an array of 512 bits or it sends to the "chunker" function to return a "list of arrays" of 512 bits
+int *preprocessMessage(char *message) {
+    // Translates the message in an array of bits, (8 bits for every character in the message)
+    int *bits = translate(message);
+    // printf("%d\n", *bits);
+    // for (int i = 0; i < 16; i++) {
+    //     printf("%d\n", *(bits + i) - '0');
+    // }
+    // # Takes the length of the array of bits (multiple of eight)
+    int lenght = strlen(message) * 8;
+    // Translates the length of the array to binary
+    char *binLenght = bin(lenght);
+    // Chops off the 0b binary indicator (e.g. 0b11000 -> 11000)
+    // Fills the beggining of the string with 0's until it has a length of 64 characters (64 bits)
+    // Converts each bit of the string to an integer
+    // Appends each of the converted bits to an array
+    char *choppedBinLenght = malloc(strlen(binLenght) - 2);
+    int counter = 0;
+    for (int i = 0; i < strlen(binLenght); i++) {
+        if (i >= 2) {
+            choppedBinLenght[counter] = *(binLenght + i);
+            counter++;
+        }
+    }
+    counter = 0;
+    char *filledBinLenght = zfill(choppedBinLenght, 64);
+    // printf("%s\n", choppedBinLenght);
+    free(choppedBinLenght);
+    int messageLen[64];
+    // printf("%s\n", "opa");
+    // printf("%d\n", strlen(filledBinLenght));
+    // printf("%s\n", filledBinLenght);
+    for (int i = 0; i < 64; i++) {
+        messageLen[i] = (int) *(filledBinLenght + i);
+    }
+    // for (int i = 0; i < strlen(filledBinLenght); i++) {
+    //     printf("%c", messageLen[i]);
+    // }
+    // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+    // If the length of bits of the translated input is less than 448
+    if (lenght < 448) {
+        // printf("%d\n", lenght);
+        // printf("%s\n", "opa");
+        // Appends a single 1 in the end of the array of bits of the translated input
+        int *bitsIncreased = malloc((lenght + 1) * sizeof(int));
+        for (int i = 0; i < lenght; i++) {
+            *(bitsIncreased + i) = *(bits + i);
+        }
+        *(bitsIncreased + lenght) = 49;
+        // printf("%s\n", "opa");
+        // for (int i = 0; i < lenght + 1; i++) {
+        //     printf("%d\n", *(bitsIncreased + i));
+        // }
+        // printf("%d\n", lenght);
+        // Fills the end of the array with 0's until its length is equal to 448
+        bits = fillZeros(bitsIncreased, lenght + 1, 448, "LE");
+        free(bitsIncreased);
+        // for (int i = 0; i < lenght + 1; i++) {
+        //     printf("%c", bits[i]);
+        // }
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // for (int i = 0; i < strlen(filledBinLenght); i++) {
+        //     printf("%c", messageLen[i]);
+        // }
+        // Sums the "bits" variable with the "messageLen" variable
+        int *bitsMessageLenMerge = malloc(512 * sizeof(int));
+        for (int i = 0; i < 512; i++) {
+            if (i < 448) {
+                *(bitsMessageLenMerge + i) = *(bits + i);
+            } else {
+                *(bitsMessageLenMerge + i) = (int) *(messageLen + counter);
+                counter++;
+            }
+        }
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // printf("%lu\n", (lenght + 1 + 64));
+        // for (int i = 0; i < 512; i++) {
+        //     printf("%c", bitsMessageLenMerge[i]);
+        // }
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // Returns the bits variable inside a list
+        return bitsMessageLenMerge;
+    // Otherwise if the length of the bits of the translated input is equal to 448
+    } else if (lenght == 448) {
+        // printf("%s\n", "epa");
+        // Appends a single 1 in the end of the array of bits of the translated input
+        int *bitsIncreased = malloc((lenght + 1) * sizeof(int));
+        // printf("%lu\n", (lenght + 1) * sizeof(int));
+        for (int i = 0; i < lenght; i++) {
+            *(bitsIncreased + i) = *(bits + i);
+        }
+        *(bitsIncreased + lenght) = 49;
+        // for (int i = 0; i < lenght + 1; i++) {
+        //     printf("%c", bitsIncreased[i]);
+        // }
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // printf("%d", lenght + 1);
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // Fills the end of the array with 0's until its length is equal to 1024
+        bits = fillZeros(bitsIncreased, lenght + 1, 1024, "LE");
+        free(bitsIncreased);
+        // for (int i = 0; i < 1024; i++) {
+        //     printf("%c", *(bits + i));
+        // }
+        // Takes all the 64 bits of the "messageLen" array
+        // Puts those bits in the place of the last 64 bits of the "bits" array
+        counter = 960;
+        for (int i = 0; i < 64; i++) {
+            *(bits + counter) = (int) *(messageLen + i);
+            counter++;
+        }
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        //     printf("%c", *(bitsIncreased + 1024));
+        // printf("\n%s\n", "-------------------------------------------------------------------------------------------");
+        // for (int i = 0; i < strlen(filledBinLenght); i++) {
+        //     printf("%c", messageLen[i]);
+        // }
+        // for (int i = 0; i < 1024; i++) {
+        //     printf("%c", *(bits + i));
+        // }
+        // Sends the result to the "chunker" function
+        return chunker(bits, 1024, 512);
+    // Otherwise
+    } else {
+        // printf("%s\n", "ipa");
+        // Appends a single 1 in the end of the array of bits of the translated input
+        int *bitsIncreased = malloc((lenght + 1) * sizeof(int));
+        // printf("%d\n", lenght);
+        for (int i = 0; i < lenght; i++) {
+            // printf("%d\n", *(bits + i));
+            *(bitsIncreased + i) = *(bits + i);
+        }
+        *(bitsIncreased + lenght) = 49;
+
+        // Increases the size of the "bits" array while it's not divisible by 512
+        int sizeToIncrease = 1;
+        while ((lenght + sizeToIncrease) % 512 != 0) {
+            sizeToIncrease++;
+            // printf("%d\n", (lenght + sizeToIncrease) % 512);
+        }
+        sizeToIncrease -= 1;
+        // printf("%d", lenght + sizeToIncrease);
+        // printf("%d", sizeToIncrease);
+        int *biggestBits = malloc((lenght + 1 + sizeToIncrease) * sizeof(int));
+        for (int i = 0; i < lenght + 1 + sizeToIncrease; i++) {
+            if (i < lenght + 1) {
+                *(biggestBits + i) = *(bitsIncreased + i);
+            } else {
+                *(biggestBits + i) = 48;
+            }
+        }
+        free(bitsIncreased);
+        // for (int i = 0; i < lenght + 1 + sizeToIncrease; i++) {
+        //     printf("%c", *(biggestBits + i));
+        // }
+
+        // for (int i = 0; i < strlen(filledBinLenght); i++) {
+        //     printf("%c", messageLen[i]);
+        // }
+        // Takes all the 64 bits of the "message_len" array
+        // Puts those bits in the place of the last 64 bits of the "bits" array
+        counter = lenght + 1 + sizeToIncrease - 64;
+        for (int i = 0; i < 64; i++) {
+            *(biggestBits + counter) = (int) *(messageLen + i);
+            counter++;
+        }
+        // for (int i = 0; i < lenght + 1 + sizeToIncrease; i++) {
+        //     printf("%c", biggestBits[i]);
+        // }
+        // printf("%s", "");
+        // Sends the result to the "chunker" function
+        return chunker(biggestBits, lenght + 1 + sizeToIncrease, 512);
+    }
 }
